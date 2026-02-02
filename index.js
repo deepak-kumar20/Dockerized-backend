a6b382d20a372fc9808a3456bec1ed1c1a89059e
Deepak Kumar <deepakdagur0007@gmail.com>
2026-02-02 07:01:55 +0530

    multi-container architecture

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://Deepak:mypassword@mongo:27017/?authSource=admin')
+  .then(()=>console.log('Successfully connected to MongoDB'))
+  .catch(err => console.log('Connection error', err));
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hello .</h2>");
+  res.send("<h2>Hello Mongo started.</h2>");
 });
 
 app.listen(port, () => {
